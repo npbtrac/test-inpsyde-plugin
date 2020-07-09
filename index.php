ebc25c20889eabbdf1e1fb7f0fbcafcfd2480b6d
Trac Nguyen <npbtrac@yahoo.com>
2020-07-09 17:40:24 +0700

    Add Inpsyde phpcs https://github.com/inpsyde/php-coding-standards

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Bootstrap file
+ *
+ * Bootstrap file to the plugin
+ *
+ * @package test-ipsyde-plugin
+ *
  * Plugin Name: Test for Inpsyde
  * Description: Plugin for checking out using Tamara payment method
  * Author:      nptrac@yahoo.com
@@ -9,31 +15,34 @@
 use TestInpsyde\Wp\Plugin\Services\View_Service;
 use TestInpsyde\Wp\Plugin\Test_Inpsyde;
 
-
-// Use autoload if it isn't loaded before
+// Use autoload if it isn't loaded before.
 if ( ! class_exists( Test_Inpsyde::class ) ) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 }
 
 $text_domain = 'tamara';
 
-$config = [
+$config = array(
 	'base_path'   => __DIR__,
 	'base_url'    => plugins_url( null, __FILE__ ),
 	'text_domain' => $text_domain,
-	'services'    => [
-		View_Service::class => [
+	'services'    => array(
+		View_Service::class => array(
 			'text_domain' => $text_domain,
-		]
-	],
-];
+		),
+	),
+);
 
-register_activation_hook( __FILE__, [ Test_Inpsyde::class, 'activate_plugin' ] );
-register_deactivation_hook( __FILE__, [ Test_Inpsyde::class, 'deactivate_plugin' ] );
+register_activation_hook( __FILE__, array( Test_Inpsyde::class, 'activate_plugin' ) );
+register_deactivation_hook( __FILE__, array( Test_Inpsyde::class, 'deactivate_plugin' ) );
 
 // We need to set up the main instance for the plugin.
-// Use 'init' event but with low (<10) processing order to be able to execute before -> able to add other init
-add_action( 'init', function () use ( $config ) {
-	/** @noinspection PhpUnusedDeclarationInspection */
-	Test_Inpsyde::init_instance_with_config( $config );
-}, 7 );
+// Use 'init' event but with low (<10) processing order to be able to execute before -> able to add other init.
+add_action(
+	'init',
+	function () use ( $config ) {
+		// @noinspection PhpUnusedDeclarationInspection
+		Test_Inpsyde::init_instance_with_config( $config );
+	},
+	7
+);
